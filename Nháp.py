d2aa9e4198dc3e94792eb06f9c6a71c6843fea16
Administrator <minh14012001@gmail.com>
2020-05-01 15:10:42 +0700

    Commit
    Signed-off-by: Administrator <minh14012001@gmail.com>

--- Nháp.py
+++ Nháp.py
@@ -1,38 +1,13 @@
 from PIL import Image, ImageTk
-from tkinter import Tk, Label, BOTH
-from tkinter.ttk import Frame, Style
+from tkinter import *
 
-
-class Example(Frame):
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
-
-        self.parent = parent
-        self.initUI()
-
-    def initUI(self):
-        self.parent.title("Absolute positioning")
-        self.pack(fill=BOTH, expand=1)
-
-        Style().configure("TFrame", background="#030303")
-
-        bard = Image.open("back.jpg")
-        bardejov = ImageTk.PhotoImage(bard)
-        label1 = Label(self, image=bardejov)
-        label1.image = bardejov
-        label1.place(x=20, y=20)
-
-        rot = Image.open("next.jpg")
-        rotunda = ImageTk.PhotoImage(rot)
-        label2 = Label(self, image=rotunda)
-        label2.image = rotunda
-        label2.place(x=40, y=160)
 
 
 
 root = Tk()
 root.geometry("300x280+300+300")
-app = Example(root)
+app = Frame(root, )
+
 root.mainloop()
 
 
